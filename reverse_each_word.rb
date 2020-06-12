b39066cacc78f31520e83dbd042ba91e76cd5ad5
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-12 17:38:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   new_string = ""
   array = string.split
-  array.map {|word| word.reverse}
-  array.join(" ")
+  array = array.map {|word| word.reverse}
+  #array.join(" ")
   array
 end
